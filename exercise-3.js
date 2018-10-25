a6d627b25d8184bb430727a70e36d8ca7563f4f8
ahmad <ahmad.syukron6991@gmail.com>
2018-10-25 17:59:38 +0700

    baru

---
+++ exercise-3.js
@@ -0,0 +1,18 @@
+var name = 'dino';
+var peran = 'penyihir';
+
+if (name=='imam' && peran ==''){
+console.log('hai' +name + ',pilih peranmu untuk memulai game');
+}
+else if (name =='' && peran ==''){
+console.log('masukkan peran');
+}
+else if (name =='maman' && peran== 'kesatria'){
+console.log('selamat datang di dunia proxytia ' +name + 'hallo' +name  +peran +' kamu dapat memyeramg demgan senjatamu');
+}
+else if (name =='dino' && peran=='penyihir'){
+console.log('selamat datang di dunia proxytia' +name + 'hallo' +name +peran + 'kamu akan membantu temanmu yang terluka');
+}
+else if (name =='zero' && peran=='tabib'){
+console.log('selamat datang di dinia proxytia' +name + 'hallo' +name +peran + 'peran kamu akan mencipkan keajaiban untuk temanmu');
+}
---
+++ exercise-4.js
@@ -0,0 +1,11 @@
+var tanggal = 30;
+var tahun = 2020;
+
+if (tanggal <0 >31 || tahun <1900 >2019){
+console.log('invalid');
+}
+var bulan ='januari';
+switch (bulan)
+{
+case 'januari' : {console.log(tanggal +bulan+tahun);}
+}
