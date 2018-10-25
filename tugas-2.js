13c11d7a5491f7368c3fd1363ddfff95b6aa777a
ahmad <ahmad.syukron6991@gmail.com>
2018-10-25 15:02:29 +0700

    tugas week 2

---
+++ tugas-2.js
@@ -0,0 +1,19 @@
+var deret = 0;
+while(deret < 20){
+  deret +=2;
+  console.log(deret +'i love coding')
+}
+
+var deret = 20;
+while (deret >0){
+  console.log(deret +' i will become fullstack developer')
+  deret -=2;
+}
+
+//
+for(var angka = 1; angka <= 20; angka ++ ){
+  console.log(angka +'- I love coding')
+}
+for (var angka = 20; angka>=1; angka --){
+  console.log(angka + '-  will become fullstack developer')
+}
---
+++ tugas-asterik.js
@@ -0,0 +1,18 @@
+for (var baris = 0; baris < 5; baris++){
+    console.log('*')}
+
+for (var baris = 0; baris < 1; baris++){
+  var tampungan = '';
+  for(var kolom =0; kolom <5; kolom ++){
+    tampungan = tampungan+ '*'; 
+    console.log(tampungan)
+  }
+}
+
+for (var o = 0; o <5; o ++){
+  var p = '';
+  for(var z = 0; z <5; z ++){
+    p = p +'*';  
+  }
+  console.log(p) 
+}
